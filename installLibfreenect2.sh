8017f4513158e4fe2bcad52a702794ee2e0136a0
caganselim <caganselim@gmail.com>
2019-10-12 22:07:44 +0300

    Added scripts

---
+++ installLibfreenect2.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/OpenKinect/libfreenect2.git
+cd libfreenect2
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libusb-1.0-0-dev
+sudo apt-get install libturbojpeg0-dev
+sudo apt-get install libglfw3-dev
+sudo apt-get install libopenni2-dev
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/freenect2
+make
+make install
+sudo cp ../platform/linux/udev/90-kinect2.rules /etc/udev/rules.d/
+cd $HOME/libfreenect2/build
